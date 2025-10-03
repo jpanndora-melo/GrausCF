f9ca41006753909be44d634f6f771a541e462796
Jessica <jpanndora@gmail.com>
2025-10-03 01:48:52 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Digite a temperatura em graus Celsius: ");
+double Celsius = Convert.ToDouble(Console.ReadLine());
+double tempF = Celsius * 1.8 + 32;
+Console.WriteLine($"{Celsius}°C equivalem a {tempF}°F");
